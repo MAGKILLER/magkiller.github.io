26656995d4bb2285d013f47d0ff80b89ad837349
MAGKILLER <jacklishijia@gmail.com>
2016-06-28 18:00:38 -0700

    new

--- battleship/script.js
+++ battleship/script.js
@@ -1,91 +1,62 @@
-var cv ;
+var userMap = initialMaps();
 var ctx;
-var background = new Image();
-var myMap = new Array(10);
-for(var i = 0; i <= 9 ; i++ )
-    myMap[i] = new Array(10);
-for(var i = 0; i <= 9 ; i++ ){
-    for(var j = 0; j <= 9 ; j++ ){
-        myMap[i][j] = false;
-    }
-}
 
-function game(){
-    document.getElementById("body").innerHTML="<div id=\"container\"><canvas id=\"screen\" width=\"1024\" height=\"768\"></canvas><div id =\"control1\"></div><div id =\"control2\"></div></div>"
-    cv = document.getElementById("screen");
-    ctx = cv.getContext("2d");
-    background.src = "image/BKG.png";
-    background.onload = drawBackground;
-    //drawBackground();
-}
-function drawBackground(){
-    ctx.drawImage(background,0,768,1024,768,0,0,1024,768);
-    drawGrid(8,260);
+function replaceScreen(){  
+    /*for(var i =0 ; i < 10 ; i++){  
+        for(var j =0; j < 10 ; j++){   
+            console.log(userMap[i][j].toString());
+        }
+    } */ 
+    document.getElementById("screen").innerHTML = "<canvas id=\"game\" width=\"1024\" height=\"768\" onClick=\"clicked(event)\"></canvas>"
+    ctx = document.getElementById("game").getContext("2d");
+    drawGrid(8,8,250);
+    drawGrid(266,8,750);
     //drawGrid(516,260);
-    //drawMark(8,260);
+    
+
+}
+function clicked(event){
+    ctx.font="60px airborne";
+    var rect = document.getElementById("game").getBoundingClientRect();
+    var x = event.clientX - rect.left - 5;
+    var y = event.clientY - rect.top - 5;
+    console.log(x + ", " + y );
+    //ctx.fillRect(x,y,50,50);
+    ctx.fillText("Congraduation!", x,y);
 }
 
-/*function drawGrid(x,y){
-    ctx.fillStyle = "rgb(19,180,255)";
-    ctx.fillRect(x,y,500,500);
-    for(var i = 50; i <= 450 ; i+=50 ){
-        ctx.moveTo(x+i,y);
-        ctx.lineTo(x+i,y+500);
+function drawGrid(x,y,scale){
+    
+    var width = scale/11;
+    ctx.font="" + width + "px airborne";
+    ctx.fillStyle = "#DDDDFF";
+    ctx.fillRect(x+width,y+width,scale-width,scale-width);
+    ctx.strokeRect(x,y,scale,scale);
+    ctx.fillStyle = "black";
+    for(var i = 1; i <= 10 ; i++){
+        ctx.moveTo(x+i*width,y);
+        ctx.lineTo(x+i*width,y+scale);
         ctx.stroke();
+        ctx.fillText(i,x+i*width+width/20,y+width - width/10);
     }
     
-    for(var i = 50; i <= 450 ; i+=50 ){
-        ctx.moveTo(x,y+i);
-        ctx.lineTo(x+500,y+i);
+    for(var i = 1; i <= 10 ; i++){
+        ctx.moveTo(x,y+i*width);
+        ctx.lineTo(x+scale,y+i*width);
         ctx.stroke();
-    }
-}*/
-
-function drawGrid(){
-    document.getElementById("control1").innerHTML="";
-    document.getElementById("control2").innerHTML="";
-    for(var i = 0; i <= 9 ; i++ ){
-        for(var j = 0; j <= 9 ; j++ ){
-            var temp = "<button onclick=\"statusChange(this.id)\""; 
-            if(myMap[i][j])
-                temp += "class=\"attacted\" id=\"ctrL"+ i + j +"\"></button>";
-            else
-                temp += "class=\"empty\" id=\"ctrL"+ i + j +"\"></button>";
-            document.getElementById("control1").innerHTML += temp;
-        }
-        document.getElementById("control1").innerHTML += "<br>"
-    }
-    for(var i = 0; i <= 9 ; i++ ){
-        for(var j = 0; j <= 9 ; j++ ){
-            var temp = "<button onclick=\"statusChange(this.id)\""; 
-            if(myMap[i][j])
-                temp += "class=\"attacted\" id=\"ctrL"+ i + j +"\"></button>";
-            else
-                temp += "class=\"empty\" id=\"ctrL"+ i + j +"\"></button>";
-            document.getElementById("control2").innerHTML += temp;
-        }
-        document.getElementById("control2").innerHTML += "<br>"
+        ctx.fillText(String.fromCharCode(64+i),x+width/10,y+(i+1)*width - width/10);
     }
 }
 
-function statusChange(id){
-    var x = Number(id.charAt(4));
-    var y = Number(id.charAt(5));
-    myMap[x][y] = true;
-    drawGrid();
-}
-
-function drawMark(x,y){
-    var w = 50;
-    for(var i = 0; i<=9; i++){
-        if(myMap[i]){
-            ctx.fillStyle="yellow";
-            ctx.strokeStyle="black";
-            ctx.fillRect(x+i*w, y, w,w);
-            ctx.stroke();
-            
+function initialMaps(){
+    var map = new Array(10);
+    for(var i =0 ; i < 10 ; i++){
+        map[i] = new Array(10);  
+        for(var j =0; j < 10 ; j++){   
+            map[i][j] = [false,false,EMPTY];
         }
     }
+    return map;
 }
 
 function sound(src){
